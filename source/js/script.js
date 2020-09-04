d8b8e3305960e7634d3909fa4e3d1a76c5456a8f
DimonS91 <dsss.front.91@gmail.com>
2020-09-04 09:00:17 +0300

    Внесены финальные исправления v.2

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,28 @@
 var navMain = document.querySelector('.main-nav__wrapper');
-    var navToggle = document.querySelector('.main-nav__toggle');
+    var navToggle = document.querySelector('.header-menu__toggle');
+    var activateButton = document.querySelector('.no-js');
 
   navToggle.addEventListener('click', function() {
 
-    if (navToggle.classList.contains('main-nav__toggle--close')) {
-      navToggle.classList.remove('main-nav__toggle--close');
-      navToggle.classList.add('main-nav__toggle--open');
+    if (navToggle.classList.contains('header-menu__toggle-open')) {
+      navToggle.classList.remove('header-menu__toggle-open');
+      navToggle.classList.add('header-menu__toggle-close');
     } else {
-      navToggle.classList.add('main-nav__toggle--close');
-      navToggle.classList.remove('main-nav__toggle--open');
+      navToggle.classList.add('header-menu__toggle-open');
+      navToggle.classList.remove('header-menu__toggle-close');
     }
 
-    if (navMain.classList.contains('main-nav--closed')) {
-      navMain.classList.remove('main-nav--closed');
-      navMain.classList.add('main-nav--opened');
+    if (navMain.classList.contains('menu-opened')) {
+      navMain.classList.remove('menu-opened');
+      navMain.classList.add('menu-closed');
     } else {
-      navMain.classList.add('main-nav--closed');
-      navMain.classList.remove('main-nav--opened');
+      navMain.classList.add('menu-opened');
+      navMain.classList.remove('menu-closed');
     }
   });
+
+  window.onload = function () {
+    navToggle.classList.remove("no-js");
+    navMain.classList.remove('menu-opened');
+    navMain.classList.add('menu-closed');
+  };
